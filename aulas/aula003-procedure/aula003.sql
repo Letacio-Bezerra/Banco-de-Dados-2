9a0a462015e49d33ca45bc5e63109afaea8c359b
Letacio-Bezerra <letaciobezerra@gmail.com>
2025-03-10 10:09:33 -0300

    Create aula003.sql

---
+++ aulas/aula003-procedure/aula003.sql
@@ -0,0 +1,23 @@
+create database aulaProcedure
+
+use aulaProcedure
+
+create table alunos(
+	id int primary key identity(1,1),
+	nome varchar(50) not null,
+	email varchar(50),
+	endereco varchar(100) not null
+)
+
+
+create procedure inserirAluno
+@nome varchar(50),
+@email varchar(50),
+@endereco varchar(100)
+as
+insert into alunos(nome, email,endereco)
+values (@nome,@email,@endereco)
+select id,nome,email,endereco from alunos
+order by id desc
+
+exec inserirAluno 'Maria','maria@gmail.com','Osasco'
